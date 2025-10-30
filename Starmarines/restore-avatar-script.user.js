b8f975368bfe80c08b8dfb54543f04ff3958ec58
Johannes <A1asd@users.noreply.github.com>
2025-10-30 19:40:08 +0100

    Update restore-avatar-script.user.js

--- Starmarines/restore-avatar-script.user.js
+++ Starmarines/restore-avatar-script.user.js
@@ -2,9 +2,9 @@
 // @name		[Starmarines] Restore avatar on profile page
 // @description	Stellt den generierten Avatar auf der Profilseite wieder her
 // @match		*://*.starmarines.de/wod/spiel/hero/profile*
-// @version		0.1
-// @updateURL	null
-// @downloadURL	null
+// @version		0.8
+// @updateURL	https://github.com/A1asd/Userscripts/raw/refs/heads/main/Starmarines/restore-avatar-script.user.js
+// @downloadURL	https://github.com/A1asd/Userscripts/raw/refs/heads/main/Starmarines/restore-avatar-script.user.js
 // @grant		none
 // @author		A1asd
 // ==/UserScript==
